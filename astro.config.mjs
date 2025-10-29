20ab2fb72cba86a0ac5304955eec40d4d9a567e1
Sebastianidm <you@example.com>
2025-10-29 17:07:40 -0300

    feat: configuracion inicial de astro con tailwind

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
 
+// Importa la integración oficial de Astro
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  // Así se usa la integración
+  integrations: [tailwind()]
+});
